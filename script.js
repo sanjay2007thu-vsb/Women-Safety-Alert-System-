4212ec59aecf2f55b5877aad243493ca806ddb8f
Sanjay M <sanjay2007thu@gmail.com>
2026-06-04 13:53:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 function sendSOS() {
-    alert("Emergency Alert Sent!");
+
+    navigator.geolocation.getCurrentPosition(
+        function(position) {
+
+            let lat = position.coords.latitude;
+            let lon = position.coords.longitude;
+
+            alert(
+                "SOS Alert Sent!\nLatitude: " +
+                lat +
+                "\nLongitude: " +
+                lon
+            );
+
+        },
+        function() {
+            alert("Location access denied");
+        }
+    );
+
 }
